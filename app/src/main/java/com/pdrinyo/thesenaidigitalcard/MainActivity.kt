b2867a7094c2dff6a5b94ba61cd9c72eaf394516
BRT14 <00001148512287sp@al.educacao.sp.gov.br>
2026-05-13 13:13:30 -0300

    O começo da Carteirinha Digital 13/05/25

---
+++ app/src/main/java/com/pdrinyo/thesenaidigitalcard/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.pdrinyo.thesenaidigitalcard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.pdrinyo.thesenaidigitalcard.ui.theme.TheSenaiDigitalCardTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TheSenaiDigitalCardTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TheSenaiDigitalCardTheme {
+        Greeting("Android")
+    }
+}
